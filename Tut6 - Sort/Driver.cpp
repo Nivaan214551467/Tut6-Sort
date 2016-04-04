1bc23043bc34c9319392e3483ec2258a377c35e0
Nivaan214551467 <krishunduttn@gmail.com>
2016-04-04 10:37:26 +0200

    Added float array with printing template

--- Tut6 - Sort/Driver.cpp
+++ Tut6 - Sort/Driver.cpp
@@ -23,19 +23,38 @@ void sort(A x[], A y)		//template function used to sort
 
 }
 
+template <class B>
+void print(B x[],B y)
+{
+	for (int i = 0; i < y; i++)
+	{
+		cout << x[i] << endl;
+	}
+}
+
+
 
 int main()
 {
 	const int size = 5;		//number of elements in array
 
 	int array[size] = { 24, 66, 74, 2, 30 };		//array of integers
+	cout << "Unsorted array of Integers" << endl;
+	print(array,size);								//calls template function to print 
+
+
 	sort(array, size);								//calls sorting function
-
-	for (int i = 0; i < size; i++)					//prints sorted array
-	{
-		cout << array[i] << endl;
-	}
+	cout << endl << "Sorted array of Integers" << endl;
+	print(array, size);								//calls template function to print 
 
 
+	float array2[size] = { 12.66, 54.77, 87.02, 4.65, 30.78 };		//array of floats
+	cout << endl << "Unsorted array of Floats" << endl;
+	print(array2, float(size));								//calls template function to print 
 
+	sort(array2, float(size));								//sorts array of floats
+	cout << endl << "Sorted array of Floats" << endl;
+	print(array2, float(size));								//calls template function to print 
+
+	
 }
