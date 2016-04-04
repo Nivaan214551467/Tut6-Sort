ca5defb129819cf5f2a7c580b05a02b1e5652113
Nivaan214551467 <krishunduttn@gmail.com>
2016-04-04 10:52:37 +0200

    Added array of strings and edited templates

--- Tut6 - Sort/Driver.cpp
+++ Tut6 - Sort/Driver.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <string.h>
+#include <sstream>
 
 using namespace std;
 
-template <class A>
-void sort(A x[], A y)		//template function used to sort
+template <class A, class B>
+void sort(A x[], B y)		//template function used to sort
 {
 	A temp;					//temporary variable to store values when sorting
 
@@ -23,8 +25,8 @@ void sort(A x[], A y)		//template function used to sort
 
 }
 
-template <class B>
-void print(B x[],B y)
+template <class B, class C>
+void print(B x[],C y)
 {
 	for (int i = 0; i < y; i++)
 	{
@@ -50,11 +52,17 @@ int main()
 
 	float array2[size] = { 12.66, 54.77, 87.02, 4.65, 30.78 };		//array of floats
 	cout << endl << "Unsorted array of Floats" << endl;
-	print(array2, float(size));								//calls template function to print 
+	print(array2, size);								//calls template function to print 
 
-	sort(array2, float(size));								//sorts array of floats
+	sort(array2, size);								//sorts array of floats
 	cout << endl << "Sorted array of Floats" << endl;
-	print(array2, float(size));								//calls template function to print 
+	print(array2, size);								//calls template function to print 
 
-	
+	string array3[size] = { "Dog", "Cat", "Apple", "Zebra", "Monkey" }; //array of strings
+	cout << endl << "Unsorted array of String" << endl;
+	print(array3, size);								//calls template function to print 
+
+	sort(array3, size);								//sorts array of Strings
+	cout << endl << "Sorted array of String" << endl;
+	print(array3, size);								//calls template function to print 
 }
